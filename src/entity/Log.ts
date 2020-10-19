001b2aa64aa52d2fe06c41edd1c71297e7cd439c
Tao Yi <tao.yi@treelab.co>
2020-10-20 01:45:04 +0800

    add Log DTO

---
+++ src/entity/Log.ts
@@ -0,0 +1,44 @@
+import { Comparable } from "../utils/Comarator";
+
+/**
+ * @desc domain object for log data
+ */
+export class Log implements Comparable<Log> {
+  private constructor(
+    public readonly ip: string,
+    public readonly dateTime: string,
+    public readonly httpMethod: string,
+    public readonly url: string,
+    public readonly status: number,
+    public readonly responseTime: number
+  ) {}
+
+  isEqualTo(target: Log): boolean {
+    return target.responseTime === this.responseTime;
+  }
+
+  isLessThan(target: Log): boolean {
+    return this.responseTime < target.responseTime;
+  }
+  isGreaterThan(target: Log): boolean {
+    // console.log(this.responseTime, target.responseTime);
+
+    return this.responseTime > target.responseTime;
+  }
+
+  /**
+   * @desc convert a record string into log domain object
+   * @param record
+   */
+  static fromString(record: string) {
+    const metadata: string[] = record.replace('"', "").split(" ");
+    const ip = metadata[0];
+    const dateTime = metadata[1];
+    const httpMethod = metadata[2];
+    const url = metadata[3];
+    const status = parseInt(metadata[4]);
+    const responseTime = parseInt(metadata[5]);
+
+    return new Log(ip, dateTime, httpMethod, url, status, responseTime);
+  }
+}
